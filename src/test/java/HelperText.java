4119e9a579dffbe7ffb8abf7d193020b6e308b08
Andrei Railean <andreirailian91@gmai.com>
2022-11-02 14:13:30 +0200

    Added negative test case.

---
+++ src/test/java/HelperText.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class HelperText  extends TestBase{
+
+    @BeforeSuite
+    public void setUpInternal(){
+
+        super.url="https://partner.cloudrein.com/reset-password";
+    }
+
+    @Test
+    public void passResetTest(){
+        WebElement emailField = driver.findElement(By.xpath("//input[@placeholder=\"Email\"]"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys("Andre3gmail");
+        WebElement i= driver.findElement(By.cssSelector("#email_help > div"));
+        Assert.assertEquals(i.getText(), "Please enter a valid Email address");
+    }
+
+
+}
+
