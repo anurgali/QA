ac5d70a525bddf7bba459d3b05aea02be77a7e5b
GoKate <katerina.look@gmail.com>
2022-11-20 19:23:25 +0100

    signup page test positive scenario fails

--- src/test/java/SignUpPageTest.java
+++ src/test/java/SignUpPageTest.java
@@ -1,16 +1,13 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class SignUpPageTest extends TestBase{
 
@@ -20,13 +17,37 @@ public class SignUpPageTest extends TestBase{
         super.url="https://partner.cloudrein.com//sign-up";
     }
 
-    @Test
-    public void positiveScenarioTest(){
-        WebElement tag = findByXPath("//h2[@class='ant-typography StyledTypography__StyledTitle-sc-d1hytu-0 MxoCQ StyledTypography__HeadingTitle-sc-d1hytu-2 Title-sc-1n24hbd-0 StyledFormBlock__FormTitle-sc-16aqzbf-2 huyklH ndPRA bUplke']");
-        Assert.assertEquals(tag.getText(),"Sign up for a 14 Day Free Trial");
+    @Test (dataProvider = "newUserWithCsv")
+    public void positiveScenarioTest(String fname, String lname,String email,String pass){
+        WebElement firstName = findByXPath("//input[@placeholder=\"First Name\"] ");
+        Assert.assertEquals(firstName.getText(),"First Name");
+        WebElement lastName = findByXPath("//input[@placeholder=\"Last Name\"] ");
+        Assert.assertEquals(lastName.getText(),"Last Name");
+        WebElement eMail = findByXPath("//input[@placeholder=\"Email\"] ");
+        Assert.assertEquals(eMail.getText(),"Email");
+        WebElement passWord = findByXPath("//input[@placeholder=\"Password\"] ");
+        Assert.assertEquals(passWord.getText(),"Password");
+        WebElement confirmPassword = findByXPath("//input[@placeholder=\"Confirm Password\"] ");
+        Assert.assertEquals(confirmPassword.getText(),"Confirm Password");
     }
 
+    @DataProvider
+    public Iterator<Object[]> newUserWithCsv() {
+        List<Object[]> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("src/test/resources/TEL-33.csv"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] data = line.split(",");
 
+                list.add(new Object[]{data[0],data[1],data[2],data[3]});
+                line = reader.readLine();
+            }
+        } catch (IOException e){
+            TestBase.logger.error(e.getMessage(), e);
+        }
+        return list.iterator();
+    }
 
 
 
